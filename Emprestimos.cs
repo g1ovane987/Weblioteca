57d5d337d0b83c2d2f718438b24a3c739661d796
LeonardoIshida96678 <74939158+LeonardoIshida96678@users.noreply.github.com>
2020-11-30 23:44:10 -0300

    Add files via upload

---
+++ Emprestimos.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace testeDeFrontEnd1
+{
+    public partial class Emprestimos : UserControl
+    {
+        public Emprestimos()
+        {
+            InitializeComponent();
+            SidePanelForm1.Height = EmprestarExemplares.Height;
+            SidePanelForm1.Top = EmprestarExemplares.Top;
+            emprestarExemplares1.BringToFront();
+
+        }
+
+        private void EmprestarExemplares_Click(object sender, EventArgs e)
+        {
+            SidePanelForm1.Height = EmprestarExemplares.Height;
+            SidePanelForm1.Top = EmprestarExemplares.Top;
+            emprestarExemplares1.BringToFront();
+        }
+
+        private void DevolucaoExemplares_Click(object sender, EventArgs e)
+        {
+            SidePanelForm1.Height = DevolucaoExemplares.Height;
+            SidePanelForm1.Top = DevolucaoExemplares.Top;
+            devolucaoDeExemplares1.BringToFront();
+        }
+
+        private void FaturarEmprestimo_Click(object sender, EventArgs e)
+        {
+            SidePanelForm1.Height = FaturarEmprestimo.Height;
+            SidePanelForm1.Top = FaturarEmprestimo.Top;
+            faturarEmprestimo1.BringToFront();
+        }
+
+        private void ProlongarEmprestimo_Click(object sender, EventArgs e)
+        {
+            SidePanelForm1.Height = ProlongarEmprestimo.Height;
+            SidePanelForm1.Top = ProlongarEmprestimo.Top;
+            prolongarEmprestimo1.BringToFront();
+        }
+
+        private void button1AbonarMulta_Click(object sender, EventArgs e)
+        {
+            SidePanelForm1.Height = button1AbonarMulta.Height;
+            SidePanelForm1.Top = button1AbonarMulta.Top;
+            abonarMulta1.BringToFront();
+        }
+    }
+}
